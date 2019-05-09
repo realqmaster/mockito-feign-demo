61f1f2c8e7dce42fc4280b5169d47bb1d895c77e
Carlo <Carlo@192.168.1.100>
2019-05-09 23:03:21 +0200

    Removed unnecessary annotation

--- mock-demos/src/test/java/it/my/test/mockito/MockitoFeignTest.java
+++ mock-demos/src/test/java/it/my/test/mockito/MockitoFeignTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import it.my.test.MockDemosApplication;
@@ -22,7 +21,6 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = MockDemosApplication.class)
-@ActiveProfiles("test")
 public class MockitoFeignTest {
 	
 	@Autowired
